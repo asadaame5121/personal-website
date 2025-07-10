364eed9c79c4103af99ab5a3c96ce50d13a9e7a8
asadaame5121 <73892481+asadaame5121@users.noreply.github.com>
2025-07-11 00:50:05 +0900

    feat: add precompression middleware to server for improved performance

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import Server from "lume/core/server.ts";
 import notFound from "lume/middlewares/not_found.ts";
 import redirectAS2, { bridgyFed } from "lume/middlewares/redirect_as2.ts";
+import precompress from "lume/middlewares/precompress.ts";
 
 const server = new Server({
   port: 8000,
@@ -11,6 +12,7 @@ const server = new Server({
 const rewriteUrl = bridgyFed();
 server.use(redirectAS2({ rewriteUrl }));
 server.use(notFound({ page: "/404.html" }));
+server.use(precompress());
 
 server.start();
 
