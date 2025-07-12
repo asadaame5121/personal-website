dbb000ce1fd112d7802d0361f17e1a8ffe7e38b8
asadaame5121 <73892481+asadaame5121@users.noreply.github.com>
2025-07-12 16:21:04 +0900

    Compress html & CSS

--- server.ts
+++ server.ts
@@ -12,6 +12,7 @@ const server = new Server({
 const rewriteUrl = bridgyFed();
 server.use(redirectAS2({ rewriteUrl }));
 server.use(notFound({ page: "/404.html" }));
+server.use(precompress());
 
 server.start();
 
