ee2f5f0fff736903f19720fdb82524226cef2b2c
asadaame5121 <73892481+asadaame5121@users.noreply.github.com>
2025-11-22 14:26:34 +0900

    OG画像のタイトルと説明文にflexboxレイアウトを追加して中央配置を改善

--- _includes/og_images.jsx
+++ _includes/og_images.jsx
@@ -20,9 +20,33 @@ export default function ({ title, description }) {
       <svg width="75" viewBox="0 0 75 65" fill="#1A1A1A" style={{ margin: "0 75px" }}>
         <path d="M37.59.25l36.95 64H.64l36.95-64z"></path>
       </svg>
-      <div style={{ marginTop: 40, textAlign: "center", color: "#1A1A1A" }}>{title}</div>
+      <div
+        style={{
+          marginTop: 40,
+          textAlign: "center",
+          color: "#1A1A1A",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {title}
+      </div>
       {description && (
-        <div style={{ marginTop: 20, fontSize: 24, fontWeight: 400, color: "#555" }}>{description}</div>
+        <div
+          style={{
+            marginTop: 20,
+            fontSize: 24,
+            fontWeight: 400,
+            color: "#555",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          {description}
+        </div>
       )}
     </div>
   );
